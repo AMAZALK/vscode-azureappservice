977281abd0edbf2922935a40a325e51bce394e9c
alexweininger <alex.weininger@live.com>
2020-06-04 18:15:11 -0700

    Add children

--- src/explorer/trialApp/TrialAppTreeItem.ts
+++ src/explorer/trialApp/TrialAppTreeItem.ts
@@ -3,19 +3,25 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { AppSettingsTreeItem, DeploymentsTreeItem, SiteFilesTreeItem } from 'vscode-azureappservice';
 import { AzExtTreeItem, IActionContext } from 'vscode-azureextensionui';
 import { localize } from '../../localize';
 import { openUrl } from '../../utils/openUrl';
-import { requestUtils } from '../../utils/requestUtils';
 import { AzureAccountTreeItem } from '../AzureAccountTreeItem';
 import { ISiteTreeItem } from '../ISiteTreeItem';
 import { SiteTreeItemBase } from '../SiteTreeItemBase';
 import { ITrialAppMetadata } from './ITrialAppMetadata';
+import { TrialAppClient } from './TrialAppClient';
 
 export class TrialAppTreeItem extends SiteTreeItemBase implements ISiteTreeItem {
     public static contextValue: string = 'trialApp';
     public contextValue: string = TrialAppTreeItem.contextValue;
     public metadata: ITrialAppMetadata;
+    public deploymentsNode: DeploymentsTreeItem;
+    public readonly appSettingsNode: AppSettingsTreeItem;
+    public readonly client: TrialAppClient;
+
+    private readonly _siteFilesNode: SiteFilesTreeItem;
 
     public get label(): string {
         return this.metadata.siteName ? this.metadata.siteName : localize('nodeJsTrialApp', 'NodeJS Trial App');
@@ -42,33 +48,20 @@ export class TrialAppTreeItem extends SiteTreeItemBase implements ISiteTreeItem 
         return `https://${this.defaultHostName}`;
     }
 
-    private constructor(parent: AzureAccountTreeItem, metadata: ITrialAppMetadata) {
+    private constructor(parent: AzureAccountTreeItem, client: TrialAppClient) {
         super(parent);
-        this.metadata = metadata;
+        this.client = client;
+        this._siteFilesNode = new SiteFilesTreeItem(this, this.client, false);
+        this.deploymentsNode = new DeploymentsTreeItem(this, this.client, {}, {});
     }
 
     public static async createTrialAppTreeItem(parent: AzureAccountTreeItem, loginSession: string): Promise<TrialAppTreeItem> {
-        const metadata: ITrialAppMetadata = await this.getTrialAppMetaData(loginSession);
-        return new TrialAppTreeItem(parent, metadata);
+        const client: TrialAppClient = await TrialAppClient.createTrialAppClient(loginSession);
+        return new TrialAppTreeItem(parent, client);
     }
 
-    public static async getTrialAppMetaData(loginSession: string): Promise<ITrialAppMetadata> {
-        const metadataRequest: requestUtils.Request = await requestUtils.getDefaultRequest('https://tryappservice.azure.com/api/vscoderesource', undefined, 'GET');
-
-        metadataRequest.headers = {
-            accept: "*/*",
-            "accept-language": "en-US,en;q=0.9",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            cookie: `loginsession=${loginSession}`
-        };
-
-        const result: string = await requestUtils.sendRequest<string>(metadataRequest);
-        return <ITrialAppMetadata>JSON.parse(result);
-    }
     public async loadMoreChildrenImpl(_clearCache: boolean, _context: IActionContext): Promise<AzExtTreeItem[]> {
-        return [];
+        return [this.appSettingsNode, this.deploymentsNode, this._siteFilesNode];
     }
     public hasMoreChildrenImpl(): boolean {
         return false;
@@ -79,7 +72,7 @@ export class TrialAppTreeItem extends SiteTreeItemBase implements ISiteTreeItem 
     }
 
     public async refreshImpl(): Promise<void> {
-        this.metadata = await TrialAppTreeItem.getTrialAppMetaData(this.metadata.loginSession);
+        this.metadata = await TrialAppClient.getTrialAppMetaData(this.metadata.loginSession);
     }
 
     public isAncestorOfImpl?(contextValue: string | RegExp): boolean {
